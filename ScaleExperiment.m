2b39c85212ebe3653f5d14040755fa63b6c93643
MINJUN KWAK <kreateark@gmail.com>
2018-05-29 10:54:31 +0900

    feat: MIT 300 data processed

--- ScaleExperiment.m
+++ ScaleExperiment.m
@@ -1,54 +1,64 @@
-ifile = strcat('f0013', '.', 'jpg');
-uiImg = imread(ifile);
-[iSizeH iSizeW] = size(uiImg);
-iSizeW = iSizeW/3;
+fileID = fopen('timedata.txt','w');
 
-nK       = 8;   % the number of cluster
-nAlpha   = 15;  % parameter alpha
-    
-fprintf('Feature extraction\t\t\t\t'); t0=clock; t1=clock;
-uiFeature = FeatureExtraction(uiImg);
-elapse = etime(clock,t0); fprintf('%.3fs elapsed\n',elapse);
+for j=1:297
+    disp(j)
+    path='C:\Users\MINJUN KWAK\Documents\github\Saliency_Detect_CSVT2014\BenchmarkIMAGES\BenchmarkIMAGES\';
+    ifile = strcat(path,'i', num2str(j), '.jpg');
+    uiImg = imread(ifile);
+    [iSizeH iSizeW] = size(uiImg);
+    iSizeW = iSizeW/3;
 
-%%% Compactness extraction
-fprintf('Compactness extraction\t\t\t'); t0=clock;
-uiComp = Compactness(uiFeature, nK, nAlpha);
-elapse = etime(clock,t0); fprintf('%.3fs elapsed\n',elapse);
+    nK       = 8;   % the number of cluster
+    nAlpha   = 15;  % parameter alpha
 
-for i=64:8:256
-  
-    sfile1 = strcat('f0013', num2str(i), '.png');
-    
-    scale1 = 64;
-    scale2 = i;
-    
+    fprintf(fileID,'Feature extraction\t\t\t\t'); t0=clock; t1=clock;
+    uiFeature = FeatureExtraction(uiImg);
+    elapse = etime(clock,t0); fprintf(fileID,'%.3fs elapsed\n',elapse);
 
-    nBeta    = 300; % parameter beta
-    nEpsilon = 0.5; % parameter epsilon 
-    
-    %disp(i);
-    
-    uiF1 = imresize(uiFeature, [scale1,scale1], 'nearest');
-    uiF2= imresize(uiFeature, [scale2,scale2], 'nearest');
-    
-    %%% Saliency calcuation at the coarse scale
-    %fprintf('Saliency calculation (coarse)\t'); t0=clock;
-    uiSal1 = SalMeasure1(uiF1, uiComp, nBeta);
-    %elapse = etime(clock,t0); fprintf('%.3fs elapsed\n',elapse);
-    
-    %%% Saliency calcuation at the medium scale
-    %fprintf('Saliency calculation \t'); t0=clock;
-    t0=clock;
-    uiSal2 = SalMeasure2(uiF2, uiComp, uiSal1, nBeta, nEpsilon);
-    elapse = etime(clock,t0); fprintf('%d %.3fs elapsed\n',i,elapse);
-    
-    
-    
-    elapse = etime(clock,t1);
-    %fprintf('%.3fs elapsed totally!!\n',elapse);
+    %%% Compactness extraction
+    fprintf(fileID,'Compactness extraction\t\t\t'); t0=clock;
+    uiComp = Compactness(uiFeature, nK, nAlpha);
+    elapse = etime(clock,t0); fprintf(fileID,'%.3fs elapsed\n',elapse);
 
-    uiOut1 = imresize(uiSal2, [iSizeH iSizeW], 'bicubic');
+    for i=64:8:256
 
-	imwrite(uiOut1, sfile1);
+       
 
+        scale1 = 64;
+        scale2 = i;
+
+
+        nBeta    = 300; % parameter beta
+        nEpsilon = 0.5; % parameter epsilon 
+
+        %disp(i);
+
+        uiF1 = imresize(uiFeature, [scale1,scale1], 'nearest');
+        uiF2= imresize(uiFeature, [scale2,scale2], 'nearest');
+
+        %%% Saliency calcuation at the coarse scale
+        %fprintf('Saliency calculation (coarse)\t'); t0=clock;
+        uiSal1 = SalMeasure1(uiF1, uiComp, nBeta);
+        %elapse = etime(clock,t0); fprintf('%.3fs elapsed\n',elapse);
+
+        %%% Saliency calcuation at the medium scale
+        %fprintf('Saliency calculation \t'); t0=clock;
+        t0=clock;
+        uiSal2 = SalMeasure2(uiF2, uiComp, uiSal1, nBeta, nEpsilon);
+        elapse = etime(clock,t0); fprintf(fileID,'%d %.3fs elapsed\n',i,elapse);
+
+        elapse = etime(clock,t1);
+        %fprintf('%.3fs elapsed totally!!\n',elapse);
+
+        uiOut1 = imresize(uiSal2, [iSizeH iSizeW], 'bicubic');
+        
+        
+        path='C:\Users\MINJUN KWAK\Documents\github\Saliency_Detect_CSVT2014\BenchmarkIMAGES\BenchmarkIMAGES\';
+        sfile1 = strcat(path,num2str(j),'-', num2str(i), '.png');
+        
+        imwrite(uiOut1, sfile1);
+
+    end
 end
+
+fclose(fileID);
