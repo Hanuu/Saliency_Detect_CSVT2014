af470f6bf89aca5deb71d68ed7496fecae4012f3
MINJUN KWAK <kreateark@gmail.com>
2018-06-06 00:34:33 +0900

    feat: Similarity data added

--- show_similarity.m
+++ show_similarity.m
@@ -8,11 +8,23 @@
 % score=similarity(image256,image248,1);
 % fprintf('%f\n',score);
 
-for j=64:8:248
-    image1=imread(strcat('f0013',num2str(j),'.png'));
-    image2=imread(strcat('f0013',num2str(j+8),'.png'));
-    
-    score=similarity(image1,image2,1);
-    fprintf('%f %f %f\n',j,j+8,score);
+fileID = fopen('similarity_data.txt','w');
+for i=1:92
+    fprintf('%d\n',i);
+    for j=64:8:248
+        image1=imread(strcat('BenchmarkIMAGES\BenchmarkIMAGES\',num2str(i),'-',num2str(j),'.png'));
+        image2=imread(strcat('BenchmarkIMAGES\BenchmarkIMAGES\',num2str(i),'-',num2str(j+8),'.png'));
 
+        score=similarity(image1,image2,1);
+        fprintf('%f %f %f\n',j,j+8,score);
+
+    end
+    image1=imread(strcat('BenchmarkIMAGES\BenchmarkIMAGES\',num2str(i),'-',num2str(64),'.png'));
+    image2=imread(strcat('BenchmarkIMAGES\BenchmarkIMAGES\',num2str(i),'-',num2str(256),'.png'));
+
+    score=similarity(image1,image2,1);
+    fprintf('%f %f %f\n',64,256,score);
 end
+
+
+fclose(fileID);
